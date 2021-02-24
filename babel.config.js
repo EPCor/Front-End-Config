14b76cd7ec1cd551cfcf95080849a7c63416003c
zhilidali <zhilidali@qq.com>
2021-02-24 13:36:58 +0800

    upgrade dependencies

--- babel.config.js
+++ babel.config.js
@@ -13,22 +13,8 @@ const options = {
     ],
   ],
   plugins: [
-    // ||=, &&= , ??=
-    ['@babel/plugin-proposal-logical-assignment-operators'],
-    // export * as ns from "mod"
-    ['@babel/plugin-proposal-export-namespace-from'],
-    // a ?? b
-    ['@babel/plugin-proposal-nullish-coalescing-operator'],
-    // a?.b
-    ['@babel/plugin-proposal-optional-chaining'],
     // decorators
     ['@babel/plugin-proposal-decorators', { legacy: true }],
-    // class {#a () {}}
-    ['@babel/plugin-proposal-private-methods', { loose: true }],
-    // class {a; #a; static a; static #a;}
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    // import()
-    ['@babel/plugin-syntax-dynamic-import'],
     // re-use helpers
     ['@babel/plugin-transform-runtime'],
   ],
